d72121f09c840bc521520ede681595a0f739befc
John <Braveheart22@GMail.com>
2018-03-20 19:52:53 -0400

    Implement decided button

--- app/src/main/java/com/example/john/vacationpicker/MainActivity.kt
+++ app/src/main/java/com/example/john/vacationpicker/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.john.vacationpicker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val vacationSpots = arrayListOf("Disney", "Hawaii", "Paris", "Okinawa", "Sydney", "Bahamas")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val randomVacation = random.nextInt(vacationSpots.count())
+            vacationTxt.text = vacationSpots[randomVacation]
+        }
+
+
     }
 }
